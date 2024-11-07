ed8b83c6b65729d8b127460f2576ce399071de1d
Vinoth Kannan <svkn.87@gmail.com>
2024-11-07 18:52:23 +0530

    UX: use only img tag application logo blade

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,1 @@
-<div {{ $attributes->class(["flex gap-4"]) }}>
-  <img src="{{ config('lauth.logo_url') }}" alt="{{ config('app.name') }} Logo" />
-</div>
+<img src="{{ config('lauth.logo_url') }}" alt="{{ config('app.name') }} Logo" {{ $attributes }} />
