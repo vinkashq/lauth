663db979a86dc91150679e66308c3529e832b47b
Vinoth Kannan <svkn.87@gmail.com>
2024-11-08 12:38:02 +0530

    UX: move login link in signup form below the box

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -41,20 +41,20 @@
                 </div>
             @endif
 
-            <div class="flex items-center mt-4">
-                <span class="text-sm text-neutral-600 dark:text-neutral-400 hover:text-neutral-900 dark:hover:text-neutral-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accent-500 dark:focus:ring-offset-neutral-800" href="{{ route('login') }}">
-                    {{ __('Existing user?') }}
-                    <a href="{{ route('login') }}" class="font-bold text-accent-800 dark:text-accent-200">
-                        {{ __('Login') }}
-                    </a>
-                </span>
-
-                <div class="grow"></div>
-
+            <div class="flex items-center mt-4 justify-end">
                 <x-button class="ms-4">
                     {{ __('Register') }}
                 </x-button>
             </div>
         </form>
+
+        <x-slot name="footer">
+            <span class="mt-8 text-sm text-neutral-600 dark:text-neutral-400" href="{{ route('login') }}">
+                {{ __('Existing user?') }}
+                <a href="{{ route('login') }}" class="font-bold text-accent-800 dark:text-accent-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accent-500 dark:focus:ring-offset-neutral-800">
+                    {{ __('Login') }}
+                </a>
+            </span>
+        </x-slot>
     </x-authentication-card>
 </x-guest-layout>
