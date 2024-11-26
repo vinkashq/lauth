c1b56cb4efe31a63fbe9bc4d2e196719eed20c8f
Vinoth Kannan <svkn.87@gmail.com>
2024-11-26 12:08:51 +0530

    DEV: use laravel pulse to monitor application's performance and usage.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Pulse\Facades\Pulse;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (config('pulse.ignore_routes')) {
+            Pulse::ignoreRoutes();
+        }
     }
 
     /**
